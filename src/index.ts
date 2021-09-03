9dbca9e7eadb875511f3777690429e161b1f94b1
thisisommore <ommore501@gmail.com>
2021-09-03 04:20:36 +0000

    Register routes in index file

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,17 @@
-import { Server } from "socket.io";
 import { createServer } from "http";
-//TODO remove cors
+import { registerWebSockets } from "./websockets";
+import express from "express";
+import gamesRoute from "./routes/game-routes";
 import cors from "cors";
-const httpServer = createServer();
-const io = new Server(httpServer);
-const port = process.env.PORT || 3000;
-httpServer.listen(port);
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use("/game", gamesRoute);
+const httpServer = createServer(app);
 
-io.on("newHint", () => {
-  io.emit("clientNewHint", () => {});
+registerWebSockets(httpServer);
+const port = process.env.PORT || 3000;
+
+httpServer.listen(port, () => {
+  console.log(`Server listing on ${port}`);
 });
