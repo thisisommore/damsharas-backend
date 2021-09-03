a182631b03db5f1202a258a03c4e965a0d078ea7
thisisommore <ommore501@gmail.com>
2021-09-03 04:20:25 +0000

    Add websockets

---
+++ src/websockets.ts
@@ -0,0 +1,27 @@
+import { Server } from "http";
+import { Server as SocketServer, Socket } from "socket.io";
+import clues from "./data/clues";
+import { Player, runningGame, RunningGame } from "./RunningGame";
+import { PlayerType } from "./types/Player";
+
+export function registerWebSockets(httpServer: Server) {
+  const io = new SocketServer(httpServer, {
+    cors: {
+      origin: "http://localhost:3030",
+    },
+  });
+
+  io.on("connection", (socket) => {
+    socket.on("register", (type: PlayerType) => {
+      runningGame.addPlayer(new Player(type, socket));
+      if (type == "helper") {
+        runningGame.generateKeyword();
+        socket.emit("keywordGenerated", runningGame.keyword);
+        socket.emit("hintsGenerated", clues);
+      }
+    });
+    socket.on("addHints", (hints: string[]) => {
+      runningGame.addHints(hints);
+    });
+  });
+}
